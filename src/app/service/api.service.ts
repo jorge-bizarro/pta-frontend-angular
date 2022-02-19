097e103f872c3f8ac3c8cec5f14b2c3e31804952
Jorge Luis Bizarro Reyes <jorgeluisbizarroreyes@gmail.com>
2022-02-18 20:25:05 -0500

    feat: config environments

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private baseUrl: string = 'http://localhost:3000';
+  private baseUrl: string = environment.baseUrl;
 
   constructor(
     private httpClient: HttpClient
